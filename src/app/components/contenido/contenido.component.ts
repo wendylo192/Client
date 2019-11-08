d22bf67a906b364ae50c6761a1966af0676af757
SallyEstefania <sallystefania92@gmail.com>
2019-11-08 16:59:19 -0500

    Ajuste de contenido

--- src/app/components/contenido/contenido.component.ts
+++ src/app/components/contenido/contenido.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contenido.component.css']
 })
 export class ContenidoComponent implements OnInit {
-
-  constructor() { }
+	
+  mostrarTodos:boolean=false;
+  mostrarRomance:boolean=false;
+  mostrarDrama:boolean=false;
+  mostrarComedia:boolean=false;
+  mostrarAnime:boolean=false;
+  
+  constructor() {
+  }
 
   ngOnInit() {
+	  this.mostrarTodos=true;
+	  }
+  
+  mostrar(value){ 
+   this.ocultarTodosDefecto();
+   if(value==1){
+	  this.mostrarTodos = true;
+   }
+   else if(value==2){
+	  this.mostrarRomance=true; 
+   }
+   else if(value==3){
+	  this.mostrarDrama=true; 
+   }
+   else if(value ==4){
+	   this.mostrarComedia=true;
+   }
+   else if(value ==5){
+	   this.mostrarAnime=true;
+   }  
+  }
+  
+  ocultarTodosDefecto(){
+	  this.mostrarTodos=false;
+	  this.mostrarRomance=false;
+	  this.mostrarDrama=false;
+	  this.mostrarComedia=false;
+	  this.mostrarAnime=false;  
   }
 
 }
